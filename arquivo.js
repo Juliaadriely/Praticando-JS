1dcbed7b4536f0adaf70242e258a0bacd875a356
Juliaadriely <juliaadrisabino@gmail.com>
2026-03-17 15:27:00 -0300

    :tada: init: primeiro comit realizado

---
+++ arquivo.js
@@ -0,0 +1,68 @@
+let nome = "julia"
+let idade = "17"
+let profissão = "desenvolvedora de sistemas"
+let cidade = "são bernardo do campo"
+
+console.log(`meu nome é ${nome},eu tenho ${idade} anos de idade, trabalho como ${profissão} e moro em ${cidade}`);
+
+//Produto
+let produto = "chocolate"
+let preço = "5,99"
+let quantidade = "4"
+
+console.log(`Comprei ${quantidade} unidades de ${produto} por ${preço} cada`);
+
+//calculando valores 
+let valor1 = 10;
+let valor2 = 20;
+
+console.log(valor1 + valor2);
+console.log(valor1 - valor2);
+console.log(valor1 * valor2);
+console.log(valor1 / valor2);
+
+//tipos 
+let numero = "38"
+console.log(numero);
+console.log(typeof numero);
+
+let palavra = "anel"
+console.log(palavra);
+console.log(typeof palavra);
+
+let cor = "rosa"
+console.log(cor)
+console.log(typeof cor)
+
+let juliaestabebendoagua = true
+console.log(juliaestabebendoagua)
+console.log(typeof juliaestabebendoagua)
+
+let mensagem = "voce e legal"
+console.log(mensagem);
+console.log(typeof mensagem);
+
+// conversoes
+let num = "80"
+console.log(typeof num);
+let numconvertido = Number(num);
+console.log(typeof numconvertido);
+
+let string = String(numconvertido);
+console.log(typeof string);
+
+//DESAFIO
+
+let nome2 = "Julia"
+let idade2 = "17"
+let altura = "1,55"
+let peso = "52"
+let cidade2 = "são Bernardo do campo"
+let profissão2 = "Desenvolvedora de sistemas"
+
+console.log(`nome ${nome2}`);
+console.log(`idade ${idade2}`);
+console.log(`altura ${altura}`);
+console.log(`peso ${peso}`);
+console.log(`cidade ${cidade2}`);
+console.log(`profissão ${profissão2}`);
